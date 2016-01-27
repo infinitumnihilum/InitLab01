57b9c6c568c96d9a2bebbbe80ad7a530727d358e
infinitumnihilum <jrdavis1989@gmail.com>
2016-01-27 18:55:48 -0500

    Added boringthings, and initlab01 class

---
+++ src/main/java/davis/john/initlab01/InitializationLab01.java
@@ -0,0 +1,30 @@
+package davis.john.initlab01;
+
+import java.awt.*;
+
+/**
+ * Created by jrdavis on 1/27/16.
+ */
+public class InitializationLab01 {
+
+    public static void main(String[] args) {
+        BoringThing b1 = new BoringThing();
+        BoringThing b2 = new BoringThing();
+        BoringThing b3 = new BoringThing();
+        BoringThing b4 = new BoringThing();
+        BoringThing b5 = new BoringThing();
+
+
+        ColorfulThing c1 = new ColorfulThing("Red");
+        ColorfulThing c2 = new ColorfulThing("Blue");
+        ColorfulThing c3 = new ColorfulThing("Green");
+        ColorfulThing c4 = new ColorfulThing("Orange");
+        ColorfulThing c5 = new ColorfulThing("Black");
+
+
+
+
+    }
+
+
+}
