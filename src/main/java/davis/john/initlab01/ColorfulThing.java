0b25f5e970eae795bde5a08e8c9b0b92b2e26e95
infinitumnihilum <jrdavis1989@gmail.com>
2016-01-27 18:48:30 -0500

    New test file for colorful things

--- src/main/java/davis/john/initlab01/ColorfulThing.java
+++ src/main/java/davis/john/initlab01/ColorfulThing.java
@@ -4,4 +4,18 @@ package davis.john.initlab01;
  * Created by jrdavis on 1/27/16.
  */
 public class ColorfulThing {
+    String color;
+
+    public ColorfulThing(String color){
+    this.color = color;
+
+    }
+    public String getColor(){
+
+        return color;
+
+    }
+
+
+
 }
---
+++ src/test/java/davis/john/initlab01/ColorFulthingSpec.java
@@ -0,0 +1,17 @@
+package davis.john.initlab01;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by jrdavis on 1/27/16.
+ */
+public class ColorFulthingSpec {
+
+
+    @Test
+    public void getColorTest(){
+
+        ColorfulThing color = new ColorfulThing("green");
+    }
+
+}
